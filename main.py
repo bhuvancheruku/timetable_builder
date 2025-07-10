7f27d09c87bb8ac80fa976860f7f41342464ec0f
maxedout_13 <34746125+bhuvancheruku@users.noreply.github.com>
2025-07-10 17:12:42 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 from datetime import datetime
-from timetable_logic import GeneticAlgorithm, export_to_pdf
+from timetablelogic import GeneticAlgorithm, export_to_pdf
 
 st.title("Academic Timetable Generator")
 
